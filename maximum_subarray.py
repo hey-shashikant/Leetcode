1e7e073db6a61fe64972150b55421f2cfe3614f5
hey-shashikant <shashikantsoalnki041@gmail.com>
2024-07-27 13:36:09 +0530

    DP completed

---
+++ maximum_subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        summ,maxSum = 0,-2**31
+        for num in nums:
+            summ += num
+            if summ < num:
+                summ = num
+            if maxSum < summ:
+                maxSum = summ
+                
+        return maxSum
